07634603b5a67000a558c4c709d2470d43acb647
Emil Valentin Toma <evtoma@gmail.com>
2018-06-01 02:30:46 +0100

    Update legislators_birth_year.py
    working only with legislators variable, no file reading at all

--- legislators_birth_year.py
+++ legislators_birth_year.py
@@ -1,18 +1,19 @@
-with open("legislators.csv", "r") as f:
-    legislators = f.read()
-    #checking for corect reading of file:
-    print(legislators[0:53]) #last_name,first_name,birthday,gender,type,state,party
+#The trainer said they already created legislators variable, so no file read..
+#with open("legislators.csv", "r") as f:
+#    legislators = f.read()
+#    #checking for corect reading of file:
+#    print(legislators[0:53]) #last_name,first_name,birthday,gender,type,state,party
 
 # turning the file into a list of lists:
-legis_split = legislators.split('\n')
-legis_list = []
-for element in legis_split:
-    row = element.split(',')
-    legis_list.append(row)
+#legis_split = legislators.split(',')
+#legis_list = []
+#for element in legislators:
+#    row = element.split(',')
+#    legis_list.append(row)
 # checking the legis list:
 #print (legis_list[0:2])#[['last_name', 'first_name', 'birthday', 'gender', 'type', 'state', 'party'],
                        # ['Bassett', 'Richard', '1745-04-02', 'M', 'sen', 'DE', 'Anti-Administration']]
-    '''
+'''
 #trying unsuccesfully to cope with the bug
 for x in legis_list:
     if x[3] == '':
@@ -20,12 +21,12 @@ for x in legis_list:
 '''
 
 birth_day = []
-for item in legis_list:
+for item in legislators:
     #building the list of birthdays only:
     birth_day.append(item[2])
 #deleting the column header, which is text- we need numbers:
-del legis_list[0]
-del birth_day[0]
+#del legis_list[0]
+#del birth_day[0]
 #checking the parsed DOB:
 #print (birth_day[0:2]) #['1745-04-02', '1742-03-21']
 #splitting parsed birth_day column:
@@ -45,15 +46,16 @@ for lista in DOB_split:
 #birth_year.insert(0,'birth_year') -actually not clear if needed aheader or not
 
 #merging the two lists:
-for x in list(range(0,len(legis_list))):
-    legis_list[x].append(birth_year[x])
-legislators = legis_list
+for x in list(range(0,len(legislators))):
+    legislators[x].append(birth_year[x])
+print(legislators[:4])
 
 '''
-#transforming the list of lists in simple list:
+#transforming the list
+of lists in simple list:
 legislators = []
 for x in legis_list:
     for y in x:
         legislators.append(y)
         '''
-print(legislators[0:10])
+#print(legislators[0:10])
